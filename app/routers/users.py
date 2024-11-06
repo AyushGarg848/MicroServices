046a21c78ff059085462d28fca066f7e821628fd
Ayush Garg <ayush.garg@nokia.com>
2024-11-07 00:51:06 +0530

    Multiple Query and Path Parameters

--- app/routers/users.py
+++ app/routers/users.py
@@ -6,6 +6,14 @@ router = APIRouter()
 async def read_user_me():
     return {"user_id": "the current user"}
 
-@router.get("/users/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
+@router.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: str, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    item.update({"q": q})
+    if short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
