27700f9bd95a9ff5d4c208659b9f0ba940d5b7f5
Ayush Garg <ayush.garg@nokia.com>
2024-11-13 00:36:26 +0530

    Exception handling

--- app/main.py
+++ app/main.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 from .routers import items, users, models
 
 app = FastAPI()
+
+class CustomException(Exception):
+    def __init__(self, name: str, status_code: int):
+        self.name = name
+        self.status_code = status_code
+
+@app.exception_handler(CustomException)
+async def custom_exception_handler(request: Request,exc: CustomException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"message": f"Oops! {exc.name} did something!!"}
+    )
 
 @app.get("/")
 async def root():
@@ -10,6 +23,15 @@ async def root():
 @app.get("/files/{file_path:path}")
 async def read_file(file_path: str):
     return {"file_path": file_path}
+
+@app.get("/items-header/{item_id}")
+async def read_item_header(item_id: str):
+    if item_id != "foo":
+        raise CustomException(
+            status_code=418,
+            name=item_id
+        )
+    return {"item": "foo"}
 
 app.include_router(items.router)
 app.include_router(users.router)
